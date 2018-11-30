ed5067946e63ac7b4c6e0aa12e79074c002abf9b
DullBK201 <dullahanahk@gmail.com>
2018-11-30 17:59:55 +0100

    Files changed

--- DS_Project_v2/_Headers/ntheory.h
+++
@@ -1,9 +0,0 @@
-#ifndef NTHEORY_H_INCLUDED
-#define NTHEORY_H_INCLUDED
-#include <gmp.h>
-void randomPrime( mpz_t res,mp_bitcnt_t n,int reps);
-void randomNumber( mpz_t res,mpz_t n,int reps);
-void randomPrimeConditional( mpz_t res,mpz_t n,int reps);
-void eulerTotient(mpz_t res,mpz_t p,mpz_t q);
-void groupPrimeOGenerator(mpz_t p,mpz_t alpha,mp_bitcnt_t klength,int reps);
-#endif
